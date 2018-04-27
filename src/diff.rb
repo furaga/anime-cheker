58c3f6bf1c36c4b09bea6e5d4f25013f0a8906f7
DESKTOP-5J7ES5Q\furaga <furaga.fukahori@gmail.com>
2018-04-27 17:52:18 +0900

    Fix diff.rb seek json files until any diff found

--- src/diff.rb
+++ src/diff.rb
@@ -6,40 +6,47 @@ require "fileutils"
 require 'optparse'
 
 params = ARGV.getopts("", "src:", "dst:", "dir:")
-
-src_json_file_path = params["src"]
-dst_json_file_path = params["dst"]
+jsons = [params["src"], params["dst"]]
 
 if false == params["dir"].nil?
     files = Dir.glob(params["dir"] + "/*.json")
-    sorted = files.sort()
-    src_json_file_path = sorted[-2]
-    dst_json_file_path = sorted[-1]
-end
-
-if src_json_file_path.nil? || dst_json_file_path.nil?
-    STDERR.puts "USAGE: ruby diff.rb --src <src json> --dst <dst json>"
+    jsons = files.sort()
+    if jsons.length <= 1
+        STDERR.puts "ERROR: " + params["dir"] + " must contains at least 2 json files."
+        return
+    end
+elsif params["src"].nil? || params["dst"].nil?
+    STDERR.puts "ERROR: USAGE: ruby diff.rb --src <src json> --dst <dst json>"
     return
 end
 
-STDERR.puts "src " + src_json_file_path
-STDERR.puts "dst " + dst_json_file_path
-STDERR.puts "search items which appear only in " + dst_json_file_path
-
-src = open(src_json_file_path) do |io|
-    JSON.load(io)
-end
-
-dst = open(dst_json_file_path) do |io|
-    JSON.load(io)
-end
-
 all_items = []
-dst.each do |dst_item|
-    # use url as a unique key
-    found = src.find {|src_item| src_item["url"] == dst_item["url"]}
-    if found.nil?
-        all_items << dst_item
+for i in (jsons.length - 2).downto(0) do
+    src_json_file_path = jsons[i]
+    dst_json_file_path = jsons[-1]
+
+    STDERR.puts src_json_file_path + " <-> " + dst_json_file_path
+
+    src = open(src_json_file_path) do |io|
+        JSON.load(io)
+    end
+
+    dst = open(dst_json_file_path) do |io|
+        JSON.load(io)
+    end
+
+    dst.each do |dst_item|
+        # use url as a unique key
+        found = src.find {|src_item| src_item["url"] == dst_item["url"]}
+        if found.nil?
+            all_items << dst_item
+        end
+    end
+
+    if all_items.length <= 0
+        STDERR.puts "No new item found"
+    else
+        break
     end
 end
 
