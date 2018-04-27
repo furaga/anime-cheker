95f44241fe92afe5508189980b673455459f9cc6
DESKTOP-5J7ES5Q\furaga <furaga.fukahori@gmail.com>
2018-04-27 17:35:29 +0900

    run without download images

--- run_scrape.sh
+++ run_scrape.sh
@@ -3,4 +3,5 @@ cd `dirname $0`
 echo 'scraping: outputfile is ./json/*.json'
 mkdir ./scraped
 
-bundle exec ruby scrape.rb --download > ./scraped/`date +%Y%m%d%H%M%S`.json
+#bundle exec ruby scrape.rb --download > ./scraped/`date +%Y%m%d%H%M%S`.json
+bundle exec ruby scrape.rb > ./scraped/`date +%Y%m%d%H%M%S`.json
